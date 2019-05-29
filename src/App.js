c7ddd01ed15445c5f0f84d30b83d45b14d9aeab9
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2019-05-29 16:58:33 -0700

    Displaying my repos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      gists: null
+    }
+  }
+
+  componentDidMount(){
+    console.log('we here!!')
+    fetch('https://api.github.com/users/yesthisiskendra/repos')
+      .then(res => res.json())
+      .then(gists => {
+        this.setState({ gists })
+      })
+      .then(console.log('STATE', this.state))
+
+  }
+
+  render(){
+    const { gists } = this.state
+    console.log(gists)
+    return(
+      <Router>
+        <div>
+          <h1> Henlo Der </h1>
+          { gists ? (
+            gists.map(gist => (
+              <li key={gist.id}>
+                <Link to={`/g/${gist.id}`}>
+                  {gist.description || 'no description'}
+                </Link>
+              </li>
+            ))) : (
+              <div> Loading </div>
+      
+          )}
+        </div>
+        {gists && (
+          // <Route path="/g/:gistId" component={Gist}/>
+          <Route path="/g/:gistId" render={({ match }) => (
+              console.log('working here', gists),
+            // <h2>wheee</h2>
+              <Gist gistId={match.params.gistId} gists={gists} />
+            )}/>
+        )}
+        
+      </Router>
+    ) 
+  }
+}
+
+const Gist = ({ gistId, gists }) => {
+  let myGist = gists.find(g => g.id === parseInt(gistId))
+  console.log('MYGIST', myGist);
+  console.log(gistId, gists)
+  return(null)
+  return(
+    <div>
+      {myGist.description}
     </div>
-  );
+  )
 }
 
 export default App;
